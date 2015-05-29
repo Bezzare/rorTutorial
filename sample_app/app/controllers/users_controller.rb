4ae9c2acc327dd4b0b5740d430ed51cb681116cf
Bezzare <bezzarelin@gmail.com>
2015-05-30 00:05:06 +0800

    implement the user signup form(not work yet)

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    render 'new'
   end
 
   def show
     @user = User.find(params[:id])
     # debugger
   end
+
+
 end
